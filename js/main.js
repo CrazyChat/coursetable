6d7187db9ec15461ed2c1f587dcb6801bd9376e5
陈铭涛 <MinToo@chenmingtaodeMacBook-Pro.local>
2018-07-04 12:52:21 +0800

    6

--- js/main.js
+++ js/main.js
@@ -18,12 +18,12 @@ var courseSections = new Vue({
     el: '#course-sections',
     data: {
         items: [
-            { message: '1-2'},
-            { message: '3-4'},
-            { message: '5-6'},
-            { message: '7-8'},
-            { message: '9'},
-            { message: '10-11'},
+            { message: '1-2' },
+            { message: '3-4' },
+            { message: '5-6' },
+            { message: '7-8' },
+            { message: '9' },
+            { message: '10-11' },
         ]
     }
 })
@@ -52,6 +52,7 @@ const user = new Array();
 // 创建成员
 const user1 = { id: 0, name: '陈铭涛' };
 const user2 = { id: 1, name: '陈帅哥' };
+const user3 = { id: 2, name: '陈大大' };
 
 
 
@@ -62,47 +63,48 @@ const userCourses = [
      * weekday表示周几，0~6表示周一到周日
      * haveCourseWeeks表示有课的周
      */
-    { user: user1, courseId: 0, weekday: 0, haveCourseWeeks: [1, 2, 3, 4, 5, 6, 7, 10, 11, 12] }, // 陈铭涛在1~7、10~12周周一的1~2节有课
-    { user: user1, courseId: 2, weekday: 1, haveCourseWeeks: [4, 5, 6, 7, 8, 10, 11, 12] }, // 陈铭涛在5~8、10~12周周二的5-6节有课
-    { user: user2, courseId: 0, weekday: 0, haveCourseWeeks: [1, 2, 3, 4, 5, 6, 7, 10, 11, 12] }  // 彭彦滨在1~7、10~12周周一的1~2节有课
+    { user: user1, courseId: 0, weekday: 0, haveCourseWeeks: [] }, // 陈铭涛在1~7、10~12周周一的1~2节有课
+    { user: user1, courseId: 2, weekday: 1, haveCourseWeeks: [] }, // 陈铭涛在5~8、10~12周周二的5-6节有课
+    { user: user2, courseId: 0, weekday: 0, haveCourseWeeks: [1, 2, 3, 4, 5, 6, 7, 10, 11, 12] },  // 陈帅哥在1~7、10~12周周一的1~2节有课
 ]
 
-userCourses.forEach(userCourse => {
-    userCourse.haveCourseWeeks.forEach(week => {
-      courseTables[week - 1][userCourse.courseId][userCourse.weekday].push(userCourse.user)
-    })
-})
-
-console.log('courseTables -----> ', courseTables[0])
-
-/*
-// 获取填入表格的数据，并写入个人课程表
-function getValue() {
-    //m表示课时，0~5表示：1-2，3-4，5-6，7-8，9，10-11
-    for (let m = 0;m < 36; m++ ){
-        var week = [];
-        var course_star = document.getElementsByClassName('course-star')[m].value;    // 获取开始的周
-        var course_end = document.getElementsByClassName('course-end')[m].value;      // 获取截止的周
-        var course_else = document.getElementsByClassName('course-else')[m].value;    // 获取其他不连续的周
-        // 提取第几周到第几周成数组
-        if (course_star) {
-            week[0] = parseInt(course_star);
-            for ( var i = 1,l = course_end - course_star; i <= l; i++ ){
-                week[i] = parseInt(week[i-1]) + 1;
-            }
-        }
-        // 添加额外不连续的周 进数组
-        var num = course_else.match(/\d+/g);
-        if (num) {
-            for (let j = 0, l = num.length; j < l; j++) {
-                week.push(parseInt(num[j]));
-            }
-        }
-        user[m] = week;
-        
-        console.log(week);
-        console.log(user[m]);
-    }
-    console.log('user:',user);
+// 向userCourse写入数据函数
+function addCourse( user, courseId, weekday, haveCourseWeeks) {
+    userCourses.push({ user, courseId, weekday, haveCourseWeeks });
 }
-*/
+
+// 获取输入的课表并写入userCourse
+function getValue() {
+    for (let x = 0; x < 7; x++ ) {
+        for (let j = 0; j < 6; j++) {
+            let m = j + x * 6;
+            var course_star = document.getElementsByClassName('course-star')[m].value;    // 获取开始的周
+            var course_end = document.getElementsByClassName('course-end')[m].value;      // 获取截止的周
+            var course_else = document.getElementsByClassName('course-else')[m].value;    // 获取其他不连续的周
+            var week = [];
+            // 提取第几周到第几周成数组
+            if (course_star) {
+                week[0] = parseInt(course_star);
+                for ( var i = 1,l = course_end - course_star; i <= l; i++ ){
+                    week[i] = parseInt(week[i-1]) + 1;
+                }
+            }
+            // 添加额外不连续的周 进数组
+            var num = course_else.match(/\d+/g);
+            if (num) {
+                for (let j = 0, l = num.length; j < l; j++) {
+                    week.push(parseInt(num[j]));
+                }
+            }
+            addCourse(user1, j, x, week);
+        }
+    }
+    // 将userCourse所有数据写入课表courseTables
+    userCourses.forEach(userCourse => {
+        userCourse.haveCourseWeeks.forEach(week => {
+          courseTables[week - 1][userCourse.courseId][userCourse.weekday].push(userCourse.user)
+        })
+    })
+    console.log('courseTables -----> ', courseTables[0]);
+    console.log('courseTables -----> ', courseTables[1]);
+}
