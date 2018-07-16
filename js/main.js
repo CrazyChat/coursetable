0c7f358bff42e355bd4339fe30e3bef17d01eab1
Mittacy <mail@mintoochen.com>
2018-07-16 19:31:16 +0800

    Restructure getValue、addMember\

--- js/main.js
+++ js/main.js
@@ -5,12 +5,13 @@ let INPUT_NAME = document.getElementsByClassName('left-name')[0];  // 名字输�
 const WEEKS = 16 // 表示学期的周数为16周，可以设置为让用户输入
 const COURSETABLES = [] // 16个周的课程表, COURSETABLES[0]表示第一周的课表
 let LASTMEMBER = 0;    // 记录点击计算课表前最后停留的成员
+let IF_FIRST = [];       // 判断成员数据是否已经提交到了userCourses
 for (let i = 0; i < WEEKS; i++) {
     /**
      * push的是一个周课程表，假设叫做courseTable，三维数组，courseTable[i][j]是一个数组，存储着当前课程时间段内有课的人及其有课的周
      * i表示课时，取值范围为0~5表示：1-2，3-4，5-6，7-8，9，10-11
      * j表示周几，取值范围为0~6表示：周一到周日，注意：0表示周一
-     * 例子：courseTable[0][2] = [ { user: { id: 1, name: '陈铭涛' }, haveCourseWeeks: [1, 2, 3] } ] 表示陈铭涛在1~3周的周三的1-2节课有课
+     * 例子：courseTable[0][2] = [ { user: { id: 1, name: '陈铭涛' }, noneCourseWeeks: [1, 2, 3] } ] 表示陈铭涛在1~3周的周三的1-2节课有课
      */
     COURSETABLES.push([
       [ [], [], [], [], [], [], [] ],
@@ -34,83 +35,104 @@ const userCourses = [
     /**
      * courseId表示课时，0~5表示：1-2，3-4，5-6，7-8，9，10-11
      * weekday表示周几，0~6表示周一到周日
-     * haveCourseWeeks表示有课的周
+     * noneCourseWeeks表示无课的周
      */
-    { user: member[0], courseId: 0, weekday: 0, haveCourseWeeks: [] }, // 陈铭涛在1~7、10~12周周一的1~2节有课
+    // { user: member[0], courseId: 0, weekday: 0, noneCourseWeeks: [] },  陈铭涛在...周周一的1~2节无课
 ]
 
+// 交换表格并清掉数据
+function changeForm() {
+    if ($('#form1').is('.form-current') == true) {
+        // 清空表格数据
+        for(let x = 0; x < 7; x++) {
+            for (let j = 0; j < 6; j++) {
+                let m = j + x * 6;
+                document.getElementsByClassName('once-tex')[m].value = [];
+            }
+        }
+        $("#form1").attr("class", "form-content form-hide");
+        $("#form2").attr("class", "form-content form-current");
+    } else {
+        $("#form2").attr("class", "form-content form-hide");
+        $("#form1").attr("class", "form-content form-current");
+        // 清空表格数据
+        for(let x = 0; x < 7; x++) {
+            for (let j = 0; j < 6; j++) {
+                let m = j + x * 6 + 42;
+                document.getElementsByClassName('once-tex')[m].value = [];
+            }
+        }
+    }
+    return;
+}
 
 // 向userCourse写入数据函数
-function addCourse( user, courseId, weekday, haveCourseWeeks) {
-    userCourses.push({ user, courseId, weekday, haveCourseWeeks });
+function addCourse( user, courseId, weekday, noneCourseWeeks) {
+    userCourses.push({ user, courseId, weekday, noneCourseWeeks });
 }
 
 // 获取输入的课表并写入userCourse
 function getValue() {
-    for (let w = 0; w <= CURRENTMEMBER; w++ ){
-        for (let x = 0; x < 7; x++ ) {
-            for (let j = 0; j < 6; j++) {
-                let m = j + x * 6 + w * 42;
-                let course_star = document.getElementsByClassName('course-star')[m].value;    // 获取开始的周
-                let course_end = document.getElementsByClassName('course-end')[m].value;      // 获取截止的周
-                let course_else = document.getElementsByClassName('course-else')[m].value;    // 获取其他不连续的周
-                let week = [];
-                let noneweek = [];
-                // 提取第几周到第几周成数组
-                if (course_star) {
-                    week[0] = parseInt(course_star);
-                    for ( let i = 1,l = course_end - course_star; i <= l; i++ ){
-                        week[i] = parseInt(week[i-1]) + 1;
-                    }
+    for (let x = 0; x < 7; x++ ) {
+        for (let j = 0; j < 6; j++) {
+            let m = j + x * 6;
+            let course_star = document.getElementsByClassName('course-star')[m].value;    // 获取开始的周
+            let course_end = document.getElementsByClassName('course-end')[m].value;      // 获取截止的周
+            let course_else = document.getElementsByClassName('course-else')[m].value;    // 获取其他不连续的周
+            let week = [];
+            let noneweek = [];
+            // 提取第几周到第几周成数组
+            if (course_star) {
+                week[0] = parseInt(course_star);
+                for ( let i = 1,l = course_end - course_star; i <= l; i++ ){
+                    week[i] = parseInt(week[i-1]) + 1;
                 }
-                // 添加额外不连续的周 进数组
-                let num = course_else.match(/\d+/g);
-                if (num) {
-                    for (let j = 0, l = num.length; j < l; j++) {
-                        week.push(parseInt(num[j]));
-                    }
-                }
-                // 不用上课的周的数组
-                for (let t = 1; t < WEEKS+1; t++) {
-                    if( week.indexOf(t) == -1 ) {
-                        noneweek.push(t);
-                    }
-                }
-                addCourse(member[w], j, x, noneweek);
             }
+            // 添加额外不连续的周 进数组
+            let num = course_else.match(/\d+/g);
+            if (num) {
+                for (let j = 0, l = num.length; j < l; j++) {
+                    week.push(parseInt(num[j]));
+                }
+            }
+            // 不用上课的周的数组
+            for (let t = 1; t < WEEKS+1; t++) {
+                if( week.indexOf(t) == -1 ) {
+                    noneweek.push(t);
+                }
+            }
+            userCourses.push({ user: member[w], courseId: j, weekday: x, noneCourseWeeks: noneweek });
         }
     }
 }
 
 // 添加新成员按钮功能
 function addMember(){
-    if ((member.length < MAXMEMBER) || (member.indexOf('') !== -1)) {
-        // 输入新建成员的姓名
-        let str = window.prompt("请输入姓名:","");
-        // 判断是否已经输入名字，然后新建成员表格滑下显示
-        if (str) {
-            if (IF_INTRODUCE) {
-                document.getElementsByClassName('introduce')[0].className = 'introduce form-hide';
-                document.getElementsByClassName('form-content')[0].className = "form-content form-current";
-                INPUT_NAME.value = str;
-                IF_INTRODUCE = false;
+    // 输入新建成员的姓名
+    let str = window.prompt("请输入姓名:","");
+    // 判断是否已经输入名字，然后新建成员表格滑下显示
+    if (str) {
+        if (IF_INTRODUCE) {
+            document.getElementsByClassName('introduce')[0].className = 'introduce form-hide';
+            $("#form1").attr("class", "form-content form-current");
+            INPUT_NAME.value = str;
+            IF_INTRODUCE = false;
+        } else {
+            getValue();
+            IF_FIRST[CURRENTMEMBER] = 1;    // 数据写入到userCourses里了
+            // 交换表格并清掉数据
+            changeForm();
+            // 判断有无哪儿删除了成员的课表没用上并使CURRENTMEMBER跳到当前成员
+            if (member.indexOf('') !== -1) {
+                CURRENTMEMBER = member.indexOf('');
             } else {
-                document.getElementsByClassName('form-content')[CURRENTMEMBER].className = "form-content form-hide";
-                // 判断有无哪儿删除了成员的课表没用上并使CURRENTMEMBER跳到当前成员
-                if (member.indexOf('') !== -1) {
-                    CURRENTMEMBER = member.indexOf('');
-                } else {
-                    CURRENTMEMBER = CURRENTMEMBER + 1;
-                }
-                INPUT_NAME.value = str;
-                document.getElementsByClassName('form-content')[CURRENTMEMBER].className = "form-content form-current";
+                CURRENTMEMBER = CURRENTMEMBER + 1;
             }
-            document.getElementsByClassName('member')[CURRENTMEMBER].innerHTML = str;
-            // 新建对象
-            member[CURRENTMEMBER] = str;
+            INPUT_NAME.value = str;
         }
-    } else {
-        alert('最多只能添加20名成员，若想添加更多成员请联系作者修改，造成不便敬请谅解！');
+        document.getElementsByClassName('member')[CURRENTMEMBER].innerHTML = str;
+        // 新建对象
+        member[CURRENTMEMBER] = str;
     }
 }
 
@@ -182,7 +204,7 @@ function NoCourseTable() {
     getValue();
     // 将userCourse所有数据写入课表COURSETABLES
     userCourses.forEach(userCourse => {
-        userCourse.haveCourseWeeks.forEach(week => {
+        userCourse.noneCourseWeeks.forEach(week => {
           COURSETABLES[week - 1][userCourse.courseId][userCourse.weekday].push(userCourse.user);
         })
     })
