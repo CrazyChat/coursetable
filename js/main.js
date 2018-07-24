7d1a626bddcdfa0ed72cbd11cb532351e92bfe59
Mittacy <mail@mintoochen.com>
2018-07-25 00:34:29 +0800

    add importStorage()

--- js/main.js
+++ js/main.js
@@ -260,9 +260,9 @@ window.onload = function() {
             oLi.innerHTML = str;
             oLi.className = "member";
             oUl.appendChild(oLi);
+            clickNav();
             // 新建对象
             member[CURRENTMEMBER] = str;
-            clickNav();
         }
     };
     // 点击删除成员按钮
@@ -451,14 +451,35 @@ function backInput() {
     deleteDate();
 }
 
+
+
+
+
+
 // 保存数据到localStorage
 function saveStorage() {
     let storage = window.localStorage;
-    storage.setItem("sWEEKS",WEEKS);
-    storage.setItem("smember_star",member_star);
-    storage.setItem("smember_end",member_end);
-    storage.setItem("smember_else",member_end);
-    storage.setItem("smember",member);
+    storage.setItem("sWEEKS", WEEKS);
+    storage.setItem("smember_star", member_star);
+    storage.setItem("smember_end", member_end);
+    storage.setItem("smember_else", member_end);
+    storage.setItem("smember", member);
     alert("保存数据成功!");
-}
-
+  }
+  
+  // 导入Storage数据
+  function importStorage() {
+    // 获取保存的数据
+    let storage = window.localStorage;
+    WEEKS = storage.getItem("sWEEKS");
+    member_star = storage.getItem("smember_star");
+    member_else = storage.getItem("smember_else");
+    member = storage.getItem("smember");
+    // 名字输入框显示“姓名”
+  
+    // 向导航栏输入名字并赋予点击响应
+  
+    // 表格全部隐藏
+  
+    // 将数据全部写入userCourse
+  }
